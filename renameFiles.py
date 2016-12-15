58225175b511ea501ab060b850478587da164c85
Folajimi Adekoya <fadekoya@temenos.com>
2016-12-16 00:09:55 +0100

    simple rename file exe

---
+++ renameFiles.py
@@ -0,0 +1,14 @@
+import os
+
+def rename_files():
+	file_list = os.listdir(r"C:\prank")
+	print(file_list)
+	saved_path = os.getcwd()
+	print("Current working Directory is "+saved_path)
+	os.chdir(r"C:\prank")
+
+	for file_name in file_list:
+		os.rename(file_name, file_name.translate(None, "0123456789") )
+
+rename_files()
+
