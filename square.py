5b90efc4f019fb0851f64342658ac4398a812361
Folajimi Adekoya <fadekoya@temenos.com>
2016-12-18 07:58:07 +0100

    draw square , circle and triangle

--- square.py
+++ square.py
@@ -1,28 +1,46 @@
 import turtle
 
 def draw_square():
-	window = turtle.Screen()
-	window.bgcolor("red")
-
+	#window = turtle.Screen()
+	#window.bgcolor("white")
+	draw=0
 	turtle.register_shape("C:\prank\poke.gif")
 	brad=turtle.Turtle()
 	brad.shape("C:\prank\poke.gif")
-	brad.color("blue")
+	brad.color("yellow")
 	brad.speed(1)
-	brad.forward(100)
-	brad.right(90)
-	brad.forward(100)
-	brad.right(90)
-	brad.forward(100)
-	brad.right(90)
-	brad.forward(100)
-	brad.right(90)
+	while draw < 4:
+		brad.forward(100)
+		brad.right(90)
+		draw+=1
 
-
+def create_background():
+	window = turtle.Screen()
+	window.bgcolor("red")
+	draw_square()
+	draw_circle()
+	draw_triangle()
 	window.exitonclick()
 
+def draw_circle():
+	angie=turtle.Turtle()
+	angie.circle(100)
+	angie.shape("arrow")
+	angie.color("blue")
+	angie.speed("3")
+
+def draw_triangle():
+	tom=turtle.Turtle()
+	tom.shape("turtle")
+	tom.color("green")
+
+	tom.right(45)
+	tom.forward(100)
+	tom.right(90)
+	tom.forward(100)
+	tom.right(135)
+	tom.forward(130)
 
 
 
-
-draw_square()
+create_background()
