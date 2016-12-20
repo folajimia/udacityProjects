30b5ad1ec2091dd27be40ec55e994a178529baf1
Folajimi Adekoya <fadekoya@temenos.com>
2016-12-20 17:48:28 +0100

    mini project sierpinski triangle

--- triangle.py
+++ triangle.py
@@ -1,6 +1,9 @@
+
+
 import turtle
 
-	
+
+
 
 tom=turtle.Turtle()
 tom.shape("turtle")
@@ -10,113 +13,40 @@ def create_background():
 	window = turtle.Screen()
 	window.bgcolor("red")
 
-	draw_triangle()
+	recursive_triangle(100,4)
+
 	window.exitonclick()
 
-
-
-def draw_triangle():
-
-	tom.setx(0)
-	tom.sety(0)
-	coordinate()
-	tom.setheading(0)
-	tom.penup()
-
-	#tom.reset()
-	tom.pendown()
-	tom.goto(100,0)
-	coordinate()
-	tom.setheading(0)
-	tom.penup()
-
-	tom.pendown()
-	tom.setx(200)
-	tom.sety(0)
-	coordinate()
-	tom.setheading(0)
-	tom.penup()
-	
-	tom.pendown()
-	tom.setx(300)
-	tom.sety(0)
-	coordinate()
-	tom.setheading(0)
-	tom.penup()
-	
-
-	tom.setx(50)
-	tom.sety(100)
-	tom.pendown()
-	coordinate()
-	tom.setheading(0)
-	tom.penup()
-
-	tom.setx(50)
-	tom.sety(100)
-	tom.pendown()
-	coordinate()
-	tom.setheading(0)
-	tom.penup()
-
-	tom.setx(250)
-	tom.sety(100)
-	tom.pendown()
-	coordinate()
-	tom.setheading(0)
-	tom.penup()
-
-	tom.setx(100)
-	tom.sety(200)
-	tom.pendown()
-	coordinate()
-	tom.setheading(0)
-	tom.penup()
-
-	tom.setx(200)
-	tom.sety(200)
-	tom.pendown()
-	coordinate()
-	tom.setheading(0)
-	tom.penup()
-
-	tom.setx(150)
-	tom.sety(300)
-	tom.pendown()
-	coordinate()
-	tom.setheading(0)
-	tom.penup()
-
-
-	
-
 	
 
 
-def coordinate():
-	tom.right(0)
-	tom.forward(100)
-	tom.left(120)
-	tom.forward(100)
-	tom.left(120)
-	tom.forward(100)
-	
-	#tom.fillcolor("violet")
 
-	tom.right(240)
-	tom.forward(50)
-
-	tom.left(120)
-	tom.forward(50)
-	tom.right(120)
-	tom.forward(50)
-	tom.left(240)
-	tom.forward(50)
+def recursive_triangle(side,recursion):
+	if recursion==1:
+		tom.begin_fill()
+		for i in range(0,3):
+			tom.forward(side)
+			tom.left(120)
+		tom.end_fill()
+	else:
+		# first triangle
+		recursive_triangle(side/2,recursion-1)
+		tom.forward(side/2)
+#second triangle
+		recursive_triangle(side/2,recursion-1)
+		tom.backward(side/2)
+		tom.left(60)
+		tom.forward(side/2)
+		tom.right(60)
+#third triangle
+		recursive_triangle(side/2,recursion-1)
+		tom.left(60)
+		tom.backward(side/2)
+		tom.right(60)
 
 
 
 	
-
 
 
 create_background()
